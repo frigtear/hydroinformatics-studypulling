1eb0fa4faff19135b033051e2de8c056a697dc97
unknown <reuben.b.sonnenschein@gmail.com>
2024-09-07 22:29:59 -0500

    Added part that gets studies

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import scholarly
+from scholarly import scholarly
 import json
 from studies import Keyword, Study
 from itertools import combinations
 from time import sleep
 
-# number of keywords to query with each domain
-NUMBER_KEYWORDS = 3
+# number of random keywords to query with each domain
+NUM_KEYWORDS = 3
 # maximum number of keywords to query google scholar per domain
 MAX_COMBINATIONS = 5
 # Seconds between each query to avoid a rate limit
 QUERY_INTERVAL = 5
+# Number of publications to get from each query
+NUM_RESULTS = 15
+
+PATH_TO_OUTPUT = "output/output.json"
+
 
 with open("config.json") as f:
     config = json.load(f)
@@ -27,19 +32,50 @@ for word in keywords:
 
 
 for domain in domains:
-    keyword = Keyword(domain, domains[domain])
+    domain = Keyword(domain, domains[domain])
     activeDomains.append(domain)
 
 
 for domain in activeDomains:
-    combos = combinations(activeKeywords, NUMBER_KEYWORDS)
+    combos = combinations(activeKeywords, NUM_KEYWORDS)
     for combo in list(combos)[:MAX_COMBINATIONS]:
-        chosen_keywords = [word.getKeyword() for word in combo] + [domain,]
-        querystring = " ".join(chosen_keywords)
-        print(chosen_keywords)
+        chosen_keywords = [word.getKeyword() for word in combo] + [domain.getKeyword(),]
+        query = " ".join(chosen_keywords)
+        search_results = scholarly.search_pubs(query)
+
+        for i in range(NUM_RESULTS):
+            try:
+                study = Study(next(search_results), combo)
+                print(f"Retrieved study {study.title}")
+            except Exception:
+                print(str(Exception))
+                continue
+
+            for keyword in combo:
+                keyword.addStudy(study)
+        
+        sleep(QUERY_INTERVAL)
+
+
+output_json = dict()
+
+
+for domain in activeDomains:
+    output_json["Domains"] = dict()
+    output_json["Domains"][domain.keyword] = [study.toJson() for study in domain.getStudies()]
+    
+for keyword in activeKeywords:
+    output_json["Keywords"] = dict()
+    output_json["Keywords"][keyword.keyword] = [study.toJson() for study in keyword.getStudies()]
+
+
+with open(PATH_TO_OUTPUT) as f:
+    json.dump(output_json)
+    
     
 
-# do every combo of 2 keywords
+
+
 
 
 
--- studies.py
+++ studies.py
@@ -1,37 +1,66 @@
 import random
 import itertools
 
-class Study:
-    def __init__(self, json):
-        bib = json["bib"]
-        self.summary = bib["abstract"]
-        self.title = bib["title"]
-        self.pub_year = bib["pub_year"]
-        self.authors = bib["author"]
 
-        self.links= (json["pub_url"], json["eprint_url"])
+
+class Study:
+    def __init__(self, json, keywords):
+        print(json)
+        self.bib = json["bib"]
+        self.summary = self.bib["abstract"]
+        self.title = self.bib["title"]
+        self.pub_year = self.bib["pub_year"]
+        self.authors = self.bib["author"]
+
+        self.links= (json["pub_url"])
         self.num_citations = json["num_citations"]
+
+        self.keywords = keywords
         
+
+
 
     def __hash__(self):
         return hash(self.title)
+    
+    
+    def __str__(self):
+        return f"{self.title} by {self.authors}"
+    
+
+    def toJson(self):
+        json = dict()
+        json["title"] = self.title
+        json["link"] = self.links
+        json["citations"] = self.num_citations
+        json["summary"] = self.summary
+        json["keywords"] = self.keywords
+
      
 
-class Keyword:
 
-    studies = set()
+class Keyword:
 
     def __init__(self, name, words):
         self.keyword = name
         self.related_keywords = words.split(", ")
+        self.studies = set()
 
 
     def addStudy(self, study):
         self.studies.add(study)
         
 
+    def getStudies(self):
+        return self.studies
+
     def getKeyword(self):
         return random.choice(self.related_keywords)
+    
+    
+    def __str__(self):
+        return str([str(study) for study in self.studies])
+
 
 
    
